96a55a1cb9f260014569995396f4dbae0fda2351
chegeian <chegeian65@gmail.com>
2024-07-07 09:15:46 +0300

    side notes

--- pass.js
+++ pass.js
@@ -39,3 +39,27 @@ function copyToClipboard(elementId) {
         alert("Failed to copy password: ", err);
     });
 }
+
+
+//  const characters = [
+//     "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+//     "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+//     "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+//     "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"
+// ];
+
+// let passEl = document.getElementById("pass1");
+// let passIl = document.getElementById("pass2");
+
+// function generate() {
+//     let password1 = "";
+//     let password2 = "";
+
+//     for (let i = 0; i < 15; i++) {
+//         password1 += characters[Math.floor(Math.random() * characters.length)];
+//         password2 += characters[Math.floor(Math.random() * characters.length)];
+//     }
+
+//     passEl.textContent = password1;
+//     passIl.textContent = password2;
+// }
